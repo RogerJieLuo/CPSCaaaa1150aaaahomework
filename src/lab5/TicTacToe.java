65a4f125527ebede9ba68a05a5ab9cf10cabc6e0
jieluo <jieluo122@gmail.com>
2017-10-03 16:07:45 -0700

    Lab 5 submission

--- src/lab5/TicTacToe.java
+++ src/lab5/TicTacToe.java
@@ -6,8 +6,16 @@ import java.util.Scanner;
 /**
  * @author by jieluo on 2017-10-03.
  * @version 2017-09-20
- * Class Lab4
+ * Class TicTacToe
  * Lab4 create a simple AI that user can play TicTacToe with.
+ * Write a java program call TicTacToe.java. This program should let the user play tic tac toe against a very basic AI.
+ * You as the player will get to go first. You should pick a position on the tic tac toe board. You program should check
+ * to see if that position is free. If it isn’t free you should be asked to pick a new position, if it is free it should
+ * place your token(“X”) in that position. Your program should check to see if you have won. If you have, the program
+ * should print out “YOU WON” else it should let the computer take its turn. The computer should pick a random free
+ * position (Should not lead to a possible infinite loop). Check tosee if the computer won. If so print out “YOU LOSE”
+ * else take your turn again and repeat until all positions are picked or someone wins.You can only use what have been
+ * taught in class up to and including week 5.
  */
 public class TicTacToe {
     public static void main(String[] args) {
@@ -16,7 +24,6 @@ public class TicTacToe {
         int position, positionAI;
         boolean won = false;
         int turn = 0;
-//        String[] player = new String[]{"AI","YOU"};
         char pToken ='X', aiToken = 'O';
         int count = 9;      // count the rest available cell
 
@@ -44,7 +51,7 @@ public class TicTacToe {
                 position = sc.nextInt();
                 x = (position - 1) / 3;
                 y = (position - 1) % 3;
-                if(board[x][y] != pToken && board[x][y] != aiToken) {
+                if(position>0 && position < 10 && board[x][y] != pToken && board[x][y] != aiToken ) {
                     validP = true;
                     board[x][y] = pToken;
                     count--;
@@ -63,10 +70,11 @@ public class TicTacToe {
                     won = true;
                 }
             }
-            //      check cross
+            //      check cross \
             if(board[0][0] == board[1][1] && board[0][0] == board[2][2]){
                 won = true;
             }
+            //      check cross /
             if(board[0][2] == board[1][1] && board[0][2] == board[2][0]){
                 won = true;
             }
@@ -93,7 +101,7 @@ public class TicTacToe {
                     }
                 }
             }
-            System.out.println(AIOption);
+            //System.out.println(AIOption); // this is line for testing the ai option
             // randomly choose one value from the string
             positionAI = Integer.parseInt(""+AIOption.charAt(rand.nextInt(AIOption.length())));
             board[(positionAI-1)/3][(positionAI-1)%3] = aiToken;
@@ -130,6 +138,8 @@ public class TicTacToe {
             if(count == 0 || won) break;
             // end of AI turn
         }
+
+        // print out the game result
         if(won){
             if(turn%2 == 1){
                 System.out.println("YOU WIN");
@@ -140,27 +150,6 @@ public class TicTacToe {
             System.out.println("It's a tie.");
         }
 
-
     }
 
-//    public static boolean ch(char[][] b){
-//        boolean won=false;
-//        for(int i=0; i < b.length;i++){
-//            if(b[i][0] == b[i][1] && b[i][0] == b[i][2]){
-//                won = true;
-//            }
-//        }
-//        for(int i = 0;i<b[0].length;i++){
-//            if(b[0][i] == b[1][i] && b[0][i] == b[2][i]){
-//                won = true;
-//            }
-//        }
-//        if(b[0][0] == b[1][1] && b[0][0] == b[2][2]){
-//            won = true;
-//        }
-//        if(b[0][2] == b[1][1] && b[0][2] == b[2][0]){
-//            won = true;
-//        }
-//        return won;
-//    }
 }
